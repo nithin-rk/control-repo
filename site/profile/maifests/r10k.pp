09ca8caeccc1e986f8eb42d00a984293e6832c1b
Nithin Ramakrishna <nithinrk@outlook.com>
2019-10-20 09:39:52 +0100

    Update r10k.pp

--- site/profile/maifests/r10k.pp
+++ site/profile/maifests/r10k.pp
@@ -1,5 +1,13 @@
 class profile:r10k {
   class { 'r10k' :
-    remote => 'https://'
+    remote => 'https://github.com/nithin-rk/control-repo',
+  }
+  class {'r10k::webhook::config' :
+    use_mcollective => false,
+    enable_ssl      => false,
+  }
+  class {'r10k::webhook' :
+    uuser => 'root',
+    group => 'root',
   }
 }
